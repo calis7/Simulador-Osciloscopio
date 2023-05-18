bc8c081353c11620883687f3003be3fe643afe3b
calis7 <camilotero89@gmail.com>
2023-05-18 23:25:08 +0200

    Edición del texto explicativo del método Main

--- src/interfazGrafica/MainLaunchAplication.java
+++ src/interfazGrafica/MainLaunchAplication.java
@@ -18,12 +18,12 @@ public class MainLaunchAplication {
 
 }
 /**
- * El código que estás utilizando en el método main es una forma común de iniciar una aplicación Swing de manera correcta y fluida. 
- * Al utilizar SwingUtilities.invokeLater(new Runnable() { ... }), estás asegurando que la creación y visualización de la interfaz 
+ * El código que se está utilizando en el método main es una forma común de iniciar una aplicación Swing de manera correcta y fluida. 
+ * Al utilizar SwingUtilities.invokeLater(new Runnable() { ... }), se está asegurando que la creación y visualización de la interfaz 
  * gráfica se realicen en el hilo de despacho de eventos de Swing (Event Dispatch Thread), que es el hilo apropiado para manejar la 
  * interfaz de usuario en Swing.
 Cuando se ejecuta el método main en Java, normalmente se ejecuta en el hilo principal (conocido como hilo "main"). 
-Si creas y muestras la interfaz gráfica directamente en el hilo principal, puede haber problemas de concurrencia y rendimiento, 
+Si se crea y muestra la interfaz gráfica directamente en el hilo principal, puede haber problemas de concurrencia y rendimiento, 
 ya que Swing no está diseñado para ser utilizado en varios hilos simultáneamente.
 
 Al usar SwingUtilities.invokeLater, le estás diciendo a Swing que programe la creación de la interfaz gráfica en el Event Dispatch Thread, 
